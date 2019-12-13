5a0a99568b5585134b82dcabcf3470062d80527f
Kahpya <nikihap@gmail.com>
2019-12-13 13:30:21 +0100

    Updates to List

--- PathfindingConsoleProject/DataStructures/GenericList.cs
+++ PathfindingConsoleProject/DataStructures/GenericList.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace PathfindingConsoleProject.DataStructures
 {
-    public class GenericList<T> : ICollection<T>
+    public class GenericList<T> : ICollection<T>, ICollection where T :IComparable<T>
     {
         public T this[int index]
         {
@@ -36,27 +36,48 @@ namespace PathfindingConsoleProject.DataStructures
         } = 0;
 
         private T[] list;
+        private int size;
 
         public bool IsReadOnly => throw new NotImplementedException();
 
-        public GenericList()
+        public object SyncRoot => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public GenericList() : this(4)
+        { }
+
+        public GenericList(int size)
         {
-            list = new T[4];
+            list = new T[size];
+            this.size = size;
         }
 
         public void Add(T item)
         {
+            list[Count] = item;
             Count++;
+
+            Resize();
         }
 
         public void Clear()
         {
+            list = new T[size];
             Count = 0;
         }
 
         public bool Contains(T item)
         {
-            
+            for (int i = 0; i < Count; i++)
+            {
+                if (item.Equals(list[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -66,17 +87,64 @@ namespace PathfindingConsoleProject.DataStructures
 
         public bool Remove(T item)
         {
-            Count--;
+            int removeIndex = -1;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (item.Equals(list[i]))
+                {
+                    removeIndex = i;
+                    break;
+                }
+            }
+
+            if (removeIndex != -1)
+            {
+                for (int j = removeIndex;  j < Count;  j++)
+                {
+                    list[j] = list[j + 1];
+                }
+
+                Count--;
+
+                return true;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            
+            for (int i = 0; i < Count; i++)
+            {
+                yield return list[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            
+            return GetEnumerator();
+        }
+
+        private void Resize()
+        {
+            if (Count == size)
+            {
+                T[] temp = new T[size];
+                list.CopyTo(temp, 0);
+
+                int newSize = size * 2;
+
+                list = new T[newSize];
+                size = newSize;
+
+                temp.CopyTo(list, 0);
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
         }
     }
 }
